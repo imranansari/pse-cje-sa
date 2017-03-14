2bdd5f71845fdaccb2c5e5d5cbfda1e67be65ee2
Kurt Madel <kmadel@mac.com>
2017-03-14 16:57:38 -0400

    added pipeline_model-def_config.groovy

---
+++ quickstart/init_04_pipeline_model-def_config.groovy
@@ -0,0 +1,20 @@
+import jenkins.model.*;
+
+import org.jenkinsci.plugins.pipeline.modeldefinition.config.GlobalConfig;
+import jenkins.scm.api.SCMSource;
+
+import java.util.logging.Logger;
+
+
+Logger logger = Logger.getLogger("init.init_04_pipeline_mode-def_config.groovy")
+File disableScript = new File(Jenkins.getInstance().getRootDir(), ".disable-pipeline-model-definition-config-script")
+if (disableScript.exists()) {
+    logger.info("DISABLE pipeline-model-definition-config script")
+    return
+}
+
+GlobalConfig globalConfig = GlobalConfiguration.all().get(GlobalConfig.class)
+globalConfig.setDockerLabel("docker-cloud")
+logger.info("setting docker label for pipeline-model-definition-config")
+
+disableScript.createNewFile()
